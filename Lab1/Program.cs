509aecdf6b63af6e0299aa5b4592bd2f6dbcf715
Юлия Лазарева <LazarevaJulia2001@outlook.com>
2022-06-03 11:15:23 +0300

    Добавьте файлы проекта.

---
+++ Lab1/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace Lab1
+{
+    internal class Program
+    {
+        private static int[] ArrayInitialize(int length)
+        {
+            int[] array = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write($"Введите значение {i} элемента массива: ");
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            return array;
+        }
+        private static void ArrayPrint(int[] array)
+        {
+            foreach(int item in array)
+            {
+                Console.Write(item+" ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            //размерность массивов
+            int array_len = 0;
+            //ввод длины первого массива
+            Console.Write("Введите колличество элементов в первом массиве: ");
+            array_len = Convert.ToInt32(Console.ReadLine());
+            //объявление и инициализация массива 
+            int[] array1 = ArrayInitialize(array_len);
+            //ввод длины второго массива
+            Console.Write("Введите колличество элементов во втором массиве: ");
+            array_len = Convert.ToInt32(Console.ReadLine());
+            //объявление и инициализация массива 
+            int[] array2 = ArrayInitialize(array_len);
+            //использовние функции UNION для объединения двух массивов
+            var rez = array1.Union(array2).ToArray();
+            //вывод результата
+            Console.Write($"\nРезультат объединения множеств: ");
+            ArrayPrint(rez);
+
+            Console.ReadLine();
+        }
+    }
+}
